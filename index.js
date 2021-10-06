df8f66c6d5dc0a81ca8a12aebf08b6ab41ffc239
Yevhen-Karpov <goldblogart@gmail.com>
2021-10-06 12:24:25 +0300

    06.10

---
+++ index.js
@@ -0,0 +1,28 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const bodyColor = document.body.style;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+startBtn.addEventListener("click", (a) => {
+  timerId = setInterval(() => {
+    bodyColor.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+  }, 1000);
+  startBtn.disabled = "true";
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  startBtn.disabled = !"true";
+});
